870d55038b9ed9f390999afdfd0e54db4d469157
mckayythomas <mckayycollins@gmail.com>
2023-04-28 23:16:43 -0500

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get("/", (request, response) => {
-//     response.send("Mariana Valeria Rodriguez Fuentes Rivera De Thomas")
-// });
+router.get("/", (request, response) => {
+    response.send("Mariana Valeria Rodriguez Fuentes Rivera De Thomas")
+});
 
 router.use("/contacts", require("./contacts"));
 
