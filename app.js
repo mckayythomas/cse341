0b1a7eeef51fb8dff31d966d5b1840ccf18dbb4e
mckayythomas <mckayycollins@gmail.com>
2023-05-08 17:21:13 -0500

    basic swagger set up

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const { connectDB } = require('./src/database/connection');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerOptions = require('./swagger.js');
 const db = require('./src/database/connection.js');
 const port = process.env.port || 3000;
+
+
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+/**
+ * @swagger
+ * /api-docs:
+ *   get:
+ *     summary: Get the Swagger API documentation
+ *     description: Endpoint to access the Swagger API documentation.
+ *     responses:
+ *       '200':
+ *         description: OK
+ */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+console.log('Api Docs made...');
 
 app
   .use(bodyParser.json())
@@ -12,8 +29,6 @@ app
     next();
   })
   .use('/', require('./src/routes'));
-
-// connectDB();
 
 db.initDb((err, mongodb) => {
   if (err) {
